a00dff11a720722bdf0bbda7f3ea78e7a0b7bfe7
SHUBHAM KUMAR <shubhamkumar@SHUBHAMs-MacBook-Air.local>
2023-04-19 10:13:14 +0530

    login page

---
+++ client/src/components/Login.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log(`Logging in with ${username} and ${password}`);
+    // TODO: Implement actual login logic here
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="max-w-md w-full bg-white p-6 rounded-md shadow-md">
+        <h2 className="text-2xl font-bold mb-6">Login Page</h2>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Username:
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="border rounded-md px-3 py-2 w-full"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="border rounded-md px-3 py-2 w-full"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            type="button"
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
